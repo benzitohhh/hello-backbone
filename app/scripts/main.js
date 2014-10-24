63de0ef174298a5c7032421f647fb84ede382dcd
Ben Immanuel <ben.immanuel@aistemos.com>
2014-10-24 13:10:36 +0100

    added readme

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,54 @@
-console.log('\'Allo \'Allo!');
+
+var object = {};
+
+_.extend(object, Backbone.Events);
+
+object.on("alert", function(msg) {
+  console.log("Triggered " + msg);
+});
+
+//object.trigger("alert", "an event");
+
+
+var Sidebar = Backbone.Model.extend({
+  promptColor: function() {
+    var cssColor = prompt("Please enter a CSS color:");
+    this.set({color: cssColor});
+  }
+});
+
+window.sidebar = new Sidebar;
+
+sidebar.on('change:color', function(model, color) {
+  $('body').css({color: 'white'});
+});
+
+//sidebar.promptColor();
+
+
+function SidebarView() {  }
+
+function MenuView() {  }
+
+var BaseView = Backbone.View.extend({
+  // Create a property where we can hold references to subviews
+  subviews: {}
+});
+
+var ContentView = BaseView.extend({
+  initialize: function() {
+    this.subviews.sidebar = new SidebarView();
+  }
+});
+
+var HeaderView = BaseView.extend({
+  initialize: function() {
+    this.subviews.menu = new MenuView();
+  }
+});
+
+var content = new ContentView();
+var header = new HeaderView();
+
+
+
