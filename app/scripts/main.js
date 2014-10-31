c649fecfaadb75cbe373a4d92f6c66754d1da669
Ben Immanuel <ben.immanuel@aistemos.com>
2014-10-31 14:26:07 +0000

    more stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,54 +1,18 @@
-
-var object = {};
-
-_.extend(object, Backbone.Events);
-
-object.on("alert", function(msg) {
-  console.log("Triggered " + msg);
-});
-
-//object.trigger("alert", "an event");
-
-
 var Sidebar = Backbone.Model.extend({
   promptColor: function() {
     var cssColor = prompt("Please enter a CSS color:");
     this.set({color: cssColor});
   }
 });
-
-window.sidebar = new Sidebar;
-
+var sidebar = new Sidebar;
 sidebar.on('change:color', function(model, color) {
   $('body').css({color: 'white'});
 });
 
-//sidebar.promptColor();
-
-
-function SidebarView() {  }
-
-function MenuView() {  }
-
-var BaseView = Backbone.View.extend({
-  // Create a property where we can hold references to subviews
-  subviews: {}
+var Book = Backbone.Model.extend({urlRoot : '/books'});
+var Library = Backbone.Collection.extend({
+  model: Book
 });
-
-var ContentView = BaseView.extend({
-  initialize: function() {
-    this.subviews.sidebar = new SidebarView();
-  }
-});
-
-var HeaderView = BaseView.extend({
-  initialize: function() {
-    this.subviews.menu = new MenuView();
-  }
-});
-
-var content = new ContentView();
-var header = new HeaderView();
-
-
+var lib = new Library();
+lib.push(new Book({title: 'war and peace'}));
 
